7a8e99c71f706c748986f6a8a07b5e74caf4cd49
JosiasAurel <ndjosiasaurel@gmail.com>
2023-03-05 08:41:53 +0100

    use component logo

--- src/Components/Logo.jsx
+++ src/Components/Logo.jsx
@@ -1,28 +1,36 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const letters = ["T-blue", "i-red", "C-black"];
 const Logo = () => {
   return (
-    <div
+    <Link
+      to="/"
       style={{
-        display: "flex",
+        textDecoration: "none",
       }}
     >
-      {letters.map((letter, idx) => {
-        const [l, color] = letter.split("-");
-        return (
-          <h1
-            key={idx}
-            style={{
-              color,
-              fontWeight: "bolder",
-            }}
-          >
-            {l}
-          </h1>
-        );
-      })}
-    </div>
+      <div
+        style={{
+          display: "flex",
+        }}
+      >
+        {letters.map((letter, idx) => {
+          const [l, color] = letter.split("-");
+          return (
+            <h1
+              key={idx}
+              style={{
+                color,
+                fontWeight: "bolder",
+              }}
+            >
+              {l}
+            </h1>
+          );
+        })}
+      </div>
+    </Link>
   );
 };
 
